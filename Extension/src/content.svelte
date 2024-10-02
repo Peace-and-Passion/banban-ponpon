eec2fd77d61c9d3c1d1de584337e276c0f937ea5
HIRANO Satoshi <happy.hirano@gmail.com>
2024-10-02 15:00:36 +0900

    indent

--- Extension/src/content.svelte
+++ Extension/src/content.svelte
@@ -8,27 +8,27 @@
 -->
 
 <script lang="ts">
-console.log('hello from the content!');
-import './style.css';
+ console.log('content.svelte started');
+ import './style.css';
 
-let multiSelectButton;
-let isSelectionMode= false;
-let clickTrackEnabled = false;
-let buttonYPosition = 50;
-let initialButtonYPosition = 50;
+ let multiSelectButton;
+ let isSelectionMode: boolean       = false;
+ let clickTrackEnabled: boolean     = false;
+ let buttonYPosition: number        = 50;
+ let initialButtonYPosition: number = 50;
 
-function enableSelectionMode() {
-  if (isSelectionMode) {
-      cancelSelectionMode();
-  } else {
-    isSelectionMode = true;
-    clickTrackEnabled = true;
-  }
-}
-function cancelSelectionMode() {
-    isSelectionMode = false;
-    clickTrackEnabled = false;
-}
+ function enableSelectionMode() {
+     if (isSelectionMode) {
+         cancelSelectionMode();
+     } else {
+         isSelectionMode = true;
+         clickTrackEnabled = true;
+     }
+ }
+ function cancelSelectionMode() {
+     isSelectionMode = false;
+     clickTrackEnabled = false;
+ }
 
 </script>
 
@@ -37,35 +37,35 @@ function cancelSelectionMode() {
 
 {#if isSelectionMode}
   <button id="done-button" on:click={cancelSelectionMode}>
-  Done
+    Done
   </button>
 {/if}
 
 <style>
-#start-button {
-    position: fixed;
-    top: 50px;
-    left: 0;
-    width: 20px;
-    height: 30px;
-    border-radius: 0 50px 50px 0;
-    overflow: hidden;
-    padding: 0;
-    border: none;
-    background-color: red;
-    z-index: 1000;
-}
+ #start-button {
+   position: fixed;
+   top: 50px;
+   left: 0;
+   width: 20px;
+   height: 30px;
+   border-radius: 0 50px 50px 0;
+   overflow: hidden;
+   padding: 0;
+   border: none;
+   background-color: red;
+   z-index: 1000;
+ }
 
-#done-button {
-    position: fixed;
-    top: 78px;
-    left: 18px;
-    padding: 4px 8px;
-    border-radius: 4px;
-    overflow: hidden;
-    border: none;
-    background-color: #f4f4fe;
-    z-index: 1000;
-    font-size: 1.2rem;
-}
+ #done-button {
+   position: fixed;
+   top: 78px;
+   left: 18px;
+   padding: 4px 8px;
+   border-radius: 4px;
+   overflow: hidden;
+   border: none;
+   background-color: #f4f4fe;
+   z-index: 1000;
+   font-size: 1.2rem;
+ }
 </style>
